97e8b0867e29ea8cdeab7f9b0fb677d39e920664
Emmanuel Douge <emmdouge@gmail.com>
2018-05-24 16:02:25 -0400

    uses var

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -6,8 +6,9 @@ class StaticController < ApplicationController
   def index
     # file_paths = Find.find(Rails.root).select { |p| /.*ngsw.json$/ =~ p }
     # puts "\n\n\n"+file_paths.to_yaml+"\n\n\n"
-    puts Rails.root
-    puts Dir.entries(Rails.root.join('app').to_s).select {|entry| File.directory? File.join(Rails.root,entry) and !(entry =='.' || entry == '..') }
+    path = Rails.root.join('app').to_s
+    puts path
+    puts Dir.entries(path).select {|entry| File.directory? File.join(path,entry) and !(entry =='.' || entry == '..') }
     render file: "/app/client/dist/prod/index.html"
   end
 end
