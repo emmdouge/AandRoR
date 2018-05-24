c8c9347ca46a4f24cad44c466db1b4e77bb6bc08
Emmanuel Douge <emmdouge@gmail.com>
2018-05-24 17:23:40 -0400

    added dist

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -17,6 +17,6 @@ class StaticController < ApplicationController
     # file_paths = Find.find(Rails.root).select { |p| /.*ngsw.json$/ =~ p }
     # puts "\n\n\n"+file_paths.to_yaml+"\n\n\n"
 
-    render file: path+'/client/src/client/index.html'
+    render file: path+'/client/dist/prod/index.html'
   end
 end
