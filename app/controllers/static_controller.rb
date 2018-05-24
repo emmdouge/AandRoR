787ffb30ee5c4ebdf8137d246a51a034089cc136
Emmanuel Douge <emmdouge@gmail.com>
2018-05-24 16:13:11 -0400

    check path

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,9 +4,12 @@ class StaticController < ApplicationController
   include ActionDispatch
 
   def index
-    path = Rails.root.join('app', 'client').to_s
+    path = Rails.root.join('app').to_s
     puts path
-    puts Dir.entries(path).select {|entry| File.directory? File.join(path,entry) and !(entry =='.' || entry == '..') }
+    puts "\n"+Dir.entries(path).select {|entry| File.directory? File.join(path,entry) and !(entry =='.' || entry == '..') }
+    puts "\n"+Dir.entries(path+'/client').select {|entry| File.directory? File.join(path+'/client',entry) and !(entry =='.' || entry == '..') }
+    puts "\n"+Dir.entries(path+'/client/dist').select {|entry| File.directory? File.join(path+'/client/dist',entry) and !(entry =='.' || entry == '..') }
+    puts "\n"+Dir.entries(path+'/client/dist/prod').select {|entry| File.directory? File.join(path+'/client/dist/prod',entry) and !(entry =='.' || entry == '..') }
     
     # file_paths = Find.find(Rails.root).select { |p| /.*ngsw.json$/ =~ p }
     # puts "\n\n\n"+file_paths.to_yaml+"\n\n\n"
