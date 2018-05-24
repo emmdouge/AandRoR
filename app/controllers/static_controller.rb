d82176c0733010e5b62800d408ab6c3127346463
Emmanuel Douge <emmdouge@gmail.com>
2018-05-24 15:14:01 -0400

    find index

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,8 +1,11 @@
+require 'find'
 class StaticController < ApplicationController
 
   include ActionDispatch
 
   def index
+    file_paths = Find.find(Rails.root).select { |p| /.*ngsw.json$/ =~ p }
+    puts "\n\n\n"+file_paths.to_yaml+"\n\n\n"
     render file: "/client/dist/prod/index.html"
   end
 end
