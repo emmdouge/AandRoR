f0ad11426e24228a1802c5d4b091423db6729331
Emmanuel Douge <emmdouge@gmail.com>
2018-05-24 16:10:03 -0400

    in client

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,11 +4,13 @@ class StaticController < ApplicationController
   include ActionDispatch
 
   def index
-    # file_paths = Find.find(Rails.root).select { |p| /.*ngsw.json$/ =~ p }
-    # puts "\n\n\n"+file_paths.to_yaml+"\n\n\n"
-    path = Rails.root.join('app').to_s
+    path = Rails.root.join('app', 'client').to_s
     puts path
     puts Dir.entries(path).select {|entry| File.directory? File.join(path,entry) and !(entry =='.' || entry == '..') }
-    render file: "/app/client/dist/prod/index.html"
+    
+    # file_paths = Find.find(Rails.root).select { |p| /.*ngsw.json$/ =~ p }
+    # puts "\n\n\n"+file_paths.to_yaml+"\n\n\n"
+
+    render file: path+'/index.html'
   end
 end
