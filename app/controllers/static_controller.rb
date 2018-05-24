ccb682766c918e9523195873e11a80d4ce7d555c
Emmanuel Douge <emmdouge@gmail.com>
2018-05-24 16:21:23 -0400

    no string conversion

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -6,10 +6,13 @@ class StaticController < ApplicationController
   def index
     path = Rails.root.join('app').to_s
     puts path
-    puts "\n"+Dir.entries(path).select {|entry| File.directory? File.join(path,entry) and !(entry =='.' || entry == '..') }
-    puts "\n"+Dir.entries(path+'/client').select {|entry| File.directory? File.join(path+'/client',entry) and !(entry =='.' || entry == '..') }
-    puts "\n"+Dir.entries(path+'/client/dist').select {|entry| File.directory? File.join(path+'/client/dist',entry) and !(entry =='.' || entry == '..') }
-    puts "\n"+Dir.entries(path+'/client/dist/prod').select {|entry| File.directory? File.join(path+'/client/dist/prod',entry) and !(entry =='.' || entry == '..') }
+    puts Dir.entries(path).select {|entry| File.directory? File.join(path,entry) and !(entry =='.' || entry == '..') }
+    puts "\n"
+    puts Dir.entries(path+'/client').select {|entry| File.directory? File.join(path+'/client',entry) and !(entry =='.' || entry == '..') }
+    puts "\n"
+    puts Dir.entries(path+'/client/dist').select {|entry| File.directory? File.join(path+'/client/dist',entry) and !(entry =='.' || entry == '..') }
+    puts "\n"
+    puts Dir.entries(path+'/client/dist/prod').select {|entry| File.directory? File.join(path+'/client/dist/prod',entry) and !(entry =='.' || entry == '..') }
     
     # file_paths = Find.find(Rails.root).select { |p| /.*ngsw.json$/ =~ p }
     # puts "\n\n\n"+file_paths.to_yaml+"\n\n\n"
